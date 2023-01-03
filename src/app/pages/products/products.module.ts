722b892ef87c3ad2634d9feca82c2919e4eca1de
Yasmin Lopes <yclopes27@gmail.com>
2023-01-02 21:40:45 -0300

    :package: chore: install ng-angular-popup

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -5,11 +5,13 @@ import { ProductsComponent } from './container/products.component';
 import { productsRoutes } from './products.routing';
 import { ProductsFormComponent } from './components/products-form/products-form.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
+import { NgToastModule } from 'ng-angular-popup';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(productsRoutes),
+    NgToastModule
   ],
   declarations: [ProductsComponent, ProductsFormComponent, ProductsListComponent],
 })
