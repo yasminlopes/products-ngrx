3102b0b3a707a15b78d9582b1d173963453038f7
yasminlopes <yclopes27@gmail.com>
2023-01-09 19:38:13 -0300

    feat: response api type

--- src/app/pages/products/models/product.ts
+++ src/app/pages/products/models/product.ts
@@ -1,7 +1,11 @@
 export interface IProduct {
-  id: number,
-  name: string,
-  category: string,
-  price: number,
-  amount: number
+  id: number;
+  name: string;
+  category: string;
+  price: number;
+  amount: number;
+}
+
+export interface ResponseApi {
+  data: IProduct;
 }
