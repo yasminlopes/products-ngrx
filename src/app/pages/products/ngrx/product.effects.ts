6950c6ecc700a4d08d6c5fe111b5dffa25dbac66
yasminlopes <yclopes27@gmail.com>
2023-01-09 18:39:52 -0300

    feat: create load all products effects

---
+++ src/app/pages/products/ngrx/product.effects.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, mergeMap, of } from 'rxjs';
+import { ProductApi } from '../api/product.api';
+import { ProductActions } from './action.types';
+
+@Injectable()
+export class ProductsEffects {
+  constructor(private actions$: Actions, private api: ProductApi) {}
+
+  loadAllProducts$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(ProductActions.loadAllProducts),
+      mergeMap(() => {
+        return this.api.getAllProducts().pipe(
+          map(
+            (payload) => ProductActions.loadProductsSuccess({ payload }),
+            catchError((error) =>
+              of(ProductActions.loadProductsFailure({ error }))
+            )
+          )
+        );
+      })
+    );
+  });
+}
